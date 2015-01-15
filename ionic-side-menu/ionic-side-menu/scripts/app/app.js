8ac27566a357940ac287e2c8e771a6fb0424b09b
Kangmo Kim <kangmo.kim@gmail.com>
2015-01-16 03:36:20 +0900

    initial commit

---
+++ ionic-side-menu/ionic-side-menu/scripts/app/app.js
@@ -0,0 +1,9 @@
+﻿angular.module('todo', ['ionic'])
+.controller('TodoCtrl', function ($scope) {
+    $scope.tasks = [
+        { title: 'Collect coins' },
+        { title: 'Eat mushrooms' },
+        { title: 'Get high to grap the flag' },
+        { title: 'Find the Princess' }
+    ];
+});
